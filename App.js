cd039f4f9a96577d86aa4cbee8e4694586d7d4b2
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-15 18:10:49 +0700

    implement navigation to Add Place

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AddPlace from './screens/AddPlace';
 import AllPlaces from './screens/AllPlaces';
+import { Text } from 'react-native';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import IconButton from './components/UI/IconButton';
 
 
 const Stack = createNativeStackNavigator();
@@ -14,7 +17,9 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen name="AllPlaces" component={AllPlaces} options={({navigation}) =>({
+            headerRight: ({tintColor}) => <IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate('AddPlace')} />
+          })} />
           <Stack.Screen name="AddPlace" component={AddPlace}/>
         </Stack.Navigator>
       </NavigationContainer>
