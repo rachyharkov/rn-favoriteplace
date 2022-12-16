afb67f45205fe039b66779ea92c36b3a500e0df8
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-16 13:24:31 +0700

    Add another cool button component

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import { Colors } from "../../constants/colors";
+import Button from "../UI/Button";
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
 
@@ -10,6 +11,8 @@ function PlaceForm() {
   function changeTitleHandler(enteredText) {
     setEnteredTitle(enteredText);
   }
+
+  function savePlaceHandler() {}
 
   return (
     <ScrollView style={styles.form}>
@@ -23,6 +26,7 @@ function PlaceForm() {
       </View>
       <ImagePicker/>
       <LocationPicker/>
+      <Button onPress={savePlaceHandler}>Add Place</Button>
     </ScrollView>
   );
 }
