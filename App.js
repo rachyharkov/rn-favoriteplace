09b6e375a4eec76bf9a4d8a35aa2f1c92ea16c12
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-16 18:56:02 +0700

    Implement my own splashscreen

--- App.js
+++ App.js
@@ -7,64 +7,94 @@ import AllPlaces from "./screens/AllPlaces";
 import IconButton from "./components/UI/IconButton";
 import { Colors } from "./constants/colors";
 import Map from "./screens/Map";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { init } from "./util/database";
 import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
 
 const Stack = createNativeStackNavigator();
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 
   const [dbInitialize, setDbInitialize] = useState(false)
 
+  const [appIsReady, setAppIsReady] = useState(false)
+
   useEffect(() => {
-    init().then(() => {
-      setDbInitialize(true)
-    }).catch(err => {
-      console.log(err)
-    })
+
+    async function prepare() {
+      try {
+        init().then(() => {
+          setDbInitialize(true)
+        }).catch(err => {
+          console.log(err)
+        })
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      }  catch(e) {
+        console.warn(e);
+      }
+      finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
   }, [])
 
-  if(!dbInitialize) {
-    return <AppLoading />
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
+  
+
+
 
   return (
     <>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerStyle: {
-            backgroundColor: Colors.primary500,
-          },
-          headerTintColor: Colors.gray700,
-          contentStyle: { backgroundColor: Colors.gray700 }
-        }}>
-          <Stack.Screen
-            name="AllPlaces"
-            component={AllPlaces}
-            options={({ navigation }) => ({
-              title: "Your Places",
-              headerRight: ({ tintColor }) => (
-                <IconButton
-                  icon="add"
-                  size={24}
-                  color={tintColor}
-                  onPress={() => navigation.navigate("AddPlace")}
-                />
-              ),
-            })}
-          />
-          <Stack.Screen
-            name="AddPlace"
-            component={AddPlace}
-            options={{
-              title: "Add Place",
-            }}
-          />
-          <Stack.Screen name="Map" component={Map} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{
+            headerStyle: {
+              backgroundColor: Colors.primary500,
+            },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 }
+          }}>
+            <Stack.Screen
+              name="AllPlaces"
+              component={AllPlaces}
+              options={({ navigation }) => ({
+                title: "Your Places",
+                headerRight: ({ tintColor }) => (
+                  <IconButton
+                    icon="add"
+                    size={24}
+                    color={tintColor}
+                    onPress={() => navigation.navigate("AddPlace")}
+                  />
+                ),
+              })}
+            />
+            <Stack.Screen
+              name="AddPlace"
+              component={AddPlace}
+              options={{
+                title: "Add Place",
+              }}
+            />
+            <Stack.Screen name="Map" component={Map} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
     </>
   );
 }
