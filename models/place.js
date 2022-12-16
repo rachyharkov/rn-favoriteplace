e85c429520233dfa6b9d538cb0570a53892a2c99
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-16 14:32:20 +0700

    Outputting list of places

--- models/place.js
+++ models/place.js
@@ -1,14 +1,13 @@
-class Place {
+export class Place {
   constructor(
     title,
     imageUri,
-    address,
     location,
   ) {
     this.title = title;
     this.imageUri = imageUri;
-    this.address = address;
-    this.location = location; // {lat: 37.78825, lng: -122.4324}
+    this.address = location.address;
+    this.location = { lat: location.lat, lng: location.lng }; // {lat: 37.78825, lng: -122.4324}
     this.id = new Date().toString() + Math.random().toString();
   }
 
